81b094812c7bbb2352f68af7b4178da4fbe9ddb9
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-01-26 07:10:30 +0300

    project initialization

---
+++ src/main/java/com/derrick/finlypal/FinlyPalApplication.java
@@ -0,0 +1,13 @@
+package com.derrick.finlypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinlyPalApplication.class, args);
+    }
+
+}
