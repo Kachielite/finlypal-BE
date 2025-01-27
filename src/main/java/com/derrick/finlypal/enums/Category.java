d8758c13c560144a3fe8cd33d56be2248ae440b7
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-01-28 02:41:27 +0300

    set up db and entities

---
+++ src/main/java/com/derrick/finlypal/enums/Category.java
@@ -0,0 +1,19 @@
+package com.derrick.finlypal.enums;
+
+public enum Category {
+    FOOD_AND_DRINKS,
+    TRANSPORTATION,
+    HOUSING,
+    UTILITIES,
+    ENTERTAINMENT,
+    HEALTHCARE,
+    EDUCATION,
+    SHOPPING,
+    TRAVEL,
+    INSURANCE,
+    SAVINGS,
+    INVESTMENTS,
+    DEBT_REPAYMENT,
+    CHARITY,
+    MISCELLANEOUS;
+}
