93f29e95b9495c4794ce5b33492d7eb23417a3cb
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-01-29 06:08:22 +0300

    implemented refresh token

---
+++ src/main/java/com/derrick/finlypal/config/JwtAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package com.derrick.finlypal.config;
+
+import com.derrick.finlypal.exception.JwtException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        JwtException.handleException(response, "Access Denied: Insufficient permission", HttpStatus.FORBIDDEN);
+
+    }
+}
--- src/main/java/com/derrick/finlypal/config/JwtAuthenticationFilter.java
+++ src/main/java/com/derrick/finlypal/config/JwtAuthenticationFilter.java
@@ -1,16 +1,13 @@
 package com.derrick.finlypal.config;
 
 import com.derrick.finlypal.util.JwtUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,7 +16,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,7 +25,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -38,19 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         String token = authHeader.substring(7);
-        String username = null;
-
-        if (!token.isBlank()) {
-            try {
-                username = jwtUtil.extractUsername(token);
-            } catch (ExpiredJwtException e) {
-                handleJWTException(response, "JWT token expired", HttpStatus.UNAUTHORIZED);
-                return;
-            } catch (MalformedJwtException e) {
-                handleJWTException(response, "Invalid token", HttpStatus.BAD_REQUEST);
-                return;
-            }
-        }
+        String username = jwtUtil.extractUsername(token);
 
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -66,14 +53,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private void handleJWTException(HttpServletResponse response, String message, HttpStatus status) throws IOException {
-        HashMap<String, Object> errors = new HashMap<>();
-        errors.put("message", message);
-
-        response.setStatus(status.value());
-        response.setContentType("application/json");
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), errors);
-    }
 }
