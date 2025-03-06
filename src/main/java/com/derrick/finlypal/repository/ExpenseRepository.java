89c75ef7cf82776bd7f5df4122641d63caaa0af7
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-06 19:49:49 +0300

    feat(expense): add repo function to sort expense by type, date, category

--- src/main/java/com/derrick/finlypal/repository/ExpenseRepository.java
+++ src/main/java/com/derrick/finlypal/repository/ExpenseRepository.java
@@ -9,128 +9,139 @@ import com.derrick.finlypal.entity.Expense;
 import com.derrick.finlypal.enums.ExpenseType;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-  Optional<Expense> findById(Long id);
+    Optional<Expense> findById(Long id);
 
-  Page<Expense> findAllByUserId(Long userId, Pageable pageable);
+    Page<Expense> findAllByUserId(Long userId, Pageable pageable);
 
-  Page<Expense> findAllByCategoryIdAndUserId(Long categoryId, Long userId, Pageable pageable);
+    Page<Expense> findAllByCategoryIdAndUserId(Long categoryId, Long userId, Pageable pageable);
 
-  Page<Expense> findAllByUserIdAndDateBetween(
-      Long userId,
-      @NotNull(message = "Start date is required") LocalDate startDate,
-      @NotNull(message = "End date is required")
-          @FutureOrPresent(message = "End date must not be less than start date")
-          LocalDate endDate,
-      Pageable pageable);
+    Page<Expense> findAllByUserIdAndDateBetween(
+            Long userId,
+            @NotNull(message = "Start date is required") LocalDate startDate,
+            @NotNull(message = "End date is required")
+            @FutureOrPresent(message = "End date must not be less than start date")
+            LocalDate endDate,
+            Pageable pageable);
 
-  Page<Expense> findAllByTypeAndUserIdOrDateBetween(
-      ExpenseType type,
-      Long userId,
-      LocalDate startDate,
-      @FutureOrPresent(message = "End date must not be less than start date") LocalDate endDate,
-      Pageable pageable);
+    Page<Expense> findAllByUserIdAndDateBetweenOrTypeOrCategoryId(
+            Long userId,
+            LocalDate startDate,
+            @FutureOrPresent(message = "End date must not be less than start date")
+            LocalDate endDate,
+            ExpenseType type,
+            Long categoryId,
+            Pageable pageable
+    );
 
-  @Query(
-      "SELECT COALESCE(SUM(e.amount), 0) FROM Expense e "
-          + "WHERE e.user.id = :userId "
-          + "AND e.date BETWEEN :startDate AND :endDate "
-          + "AND (:type IS NULL OR e.type = :type)")
-  InsightsTotalSpendDTO findTotalExpenses(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type);
+    Page<Expense> findAllByTypeAndUserIdOrDateBetween(
+            ExpenseType type,
+            Long userId,
+            LocalDate startDate,
+            @FutureOrPresent(message = "End date must not be less than start date") LocalDate endDate,
+            Pageable pageable);
 
-  @Query(
-      "SELECT CAST(COALESCE(SUM(e.amount), 0) AS bigdecimal) FROM Expense e "
-          + "WHERE e.user.id = :userId "
-          + "AND e.date BETWEEN :startDate AND :endDate "
-          + "AND (:type IS NULL OR e.type = :type)")
-  BigDecimal findTotalAmount(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type);
+    @Query(
+            "SELECT COALESCE(SUM(e.amount), 0) FROM Expense e "
+                    + "WHERE e.user.id = :userId "
+                    + "AND e.date BETWEEN :startDate AND :endDate "
+                    + "AND (:type IS NULL OR e.type = :type)")
+    InsightsTotalSpendDTO findTotalExpenses(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type);
 
-  @Query(
-      "SELECT new com.derrick.finlypal.dto.InsightsSpendByCategoryDTO("
-          + "c.name, "
-          + "SUM(e.amount), "
-          + "0) "
-          + // Set a placeholder (e.g., 0) for percentage
-          "FROM Expense e "
-          + "JOIN e.category c "
-          + "WHERE e.user.id = :userId "
-          + "AND e.date BETWEEN :startDate AND :endDate "
-          + "AND (:type IS NULL OR e.type = :type) "
-          + "GROUP BY c.name")
-  List<InsightsSpendByCategoryDTO> findTotalAmountByCategory(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type);
+    @Query(
+            "SELECT CAST(COALESCE(SUM(e.amount), 0) AS bigdecimal) FROM Expense e "
+                    + "WHERE e.user.id = :userId "
+                    + "AND e.date BETWEEN :startDate AND :endDate "
+                    + "AND (:type IS NULL OR e.type = :type)")
+    BigDecimal findTotalAmount(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type);
 
-  @Query(
-      value =
-          "SELECT new com.derrick.finlypal.dto.InsightsTopExpensesDTO(e.description, e.amount, e.date) "
-              + "FROM Expense e "
-              + "WHERE e.user.id = :userId "
-              + "AND e.date BETWEEN :startDate AND :endDate "
-              + "AND (:type IS NULL OR e.type = :type) "
-              + "ORDER BY e.amount DESC",
-      countQuery =
-          "SELECT COUNT(e) "
-              + "FROM Expense e "
-              + "WHERE e.user.id = :userId "
-              + "AND e.date BETWEEN :startDate AND :endDate "
-              + "AND (:type IS NULL OR e.type = :type)")
-  Page<InsightsTopExpensesDTO> findTopExpenses(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type,
-      Pageable pageable);
+    @Query(
+            "SELECT new com.derrick.finlypal.dto.InsightsSpendByCategoryDTO("
+                    + "c.name, "
+                    + "SUM(e.amount), "
+                    + "0) "
+                    + // Set a placeholder (e.g., 0) for percentage
+                    "FROM Expense e "
+                    + "JOIN e.category c "
+                    + "WHERE e.user.id = :userId "
+                    + "AND e.date BETWEEN :startDate AND :endDate "
+                    + "AND (:type IS NULL OR e.type = :type) "
+                    + "GROUP BY c.name")
+    List<InsightsSpendByCategoryDTO> findTotalAmountByCategory(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type);
 
-  @Query(
-      "SELECT new com.derrick.finlypal.dto.InsightsMonthlyComparisonDTO( "
-          + "TO_CHAR(e.date, 'YYYY-MM'), "
-          + // Replaces DATE_FORMAT
-          "COALESCE(SUM(e.amount), 0E0), "
-          + "e.type) "
-          + "FROM Expense e "
-          + "WHERE e.user.id = :userId "
-          + "AND e.date BETWEEN :startDate AND :endDate "
-          + "AND (:type IS NULL OR e.type = :type) "
-          + "GROUP BY TO_CHAR(e.date, 'YYYY-MM'), e.type "
-          + "ORDER BY TO_CHAR(e.date, 'YYYY-MM'), e.type")
-  List<InsightsMonthlyComparisonDTO> findMonthlyExpenseComparison(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type);
+    @Query(
+            value =
+                    "SELECT new com.derrick.finlypal.dto.InsightsTopExpensesDTO(e.description, e.amount, e.date) "
+                            + "FROM Expense e "
+                            + "WHERE e.user.id = :userId "
+                            + "AND e.date BETWEEN :startDate AND :endDate "
+                            + "AND (:type IS NULL OR e.type = :type) "
+                            + "ORDER BY e.amount DESC",
+            countQuery =
+                    "SELECT COUNT(e) "
+                            + "FROM Expense e "
+                            + "WHERE e.user.id = :userId "
+                            + "AND e.date BETWEEN :startDate AND :endDate "
+                            + "AND (:type IS NULL OR e.type = :type)")
+    Page<InsightsTopExpensesDTO> findTopExpenses(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type,
+            Pageable pageable);
 
-  @Query(
-      "SELECT new com.derrick.finlypal.dto.InsightsSpendTrendsDTO(e.date, SUM(e.amount)) "
-          + "FROM Expense e "
-          + "WHERE e.user.id = :userId "
-          + "AND e.date BETWEEN :startDate AND :endDate "
-          + "AND (:type IS NULL OR e.type = :type) "
-          + "GROUP BY e.date "
-          + "ORDER BY e.date")
-  List<InsightsSpendTrendsDTO> findSpendTrends(
-      @Param("userId") Long userId,
-      @Param("startDate") LocalDate startDate,
-      @Param("endDate") LocalDate endDate,
-      @Param("type") ExpenseType type);
+    @Query(
+            "SELECT new com.derrick.finlypal.dto.InsightsMonthlyComparisonDTO( "
+                    + "TO_CHAR(e.date, 'YYYY-MM'), "
+                    + // Replaces DATE_FORMAT
+                    "COALESCE(SUM(e.amount), 0E0), "
+                    + "e.type) "
+                    + "FROM Expense e "
+                    + "WHERE e.user.id = :userId "
+                    + "AND e.date BETWEEN :startDate AND :endDate "
+                    + "AND (:type IS NULL OR e.type = :type) "
+                    + "GROUP BY TO_CHAR(e.date, 'YYYY-MM'), e.type "
+                    + "ORDER BY TO_CHAR(e.date, 'YYYY-MM'), e.type")
+    List<InsightsMonthlyComparisonDTO> findMonthlyExpenseComparison(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type);
+
+    @Query(
+            "SELECT new com.derrick.finlypal.dto.InsightsSpendTrendsDTO(e.date, SUM(e.amount)) "
+                    + "FROM Expense e "
+                    + "WHERE e.user.id = :userId "
+                    + "AND e.date BETWEEN :startDate AND :endDate "
+                    + "AND (:type IS NULL OR e.type = :type) "
+                    + "GROUP BY e.date "
+                    + "ORDER BY e.date")
+    List<InsightsSpendTrendsDTO> findSpendTrends(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("type") ExpenseType type);
 }
