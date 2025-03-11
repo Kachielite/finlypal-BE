4fd29758911fbf4434c96b3ab3c39e1d446b2e38
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-11 23:21:37 +0300

    feat(budget): add budget request and response

---
+++ src/main/java/com/derrick/finlypal/dto/BudgetResponseDTO.java
@@ -0,0 +1,36 @@
+package com.derrick.finlypal.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(name = "BudgetResponseDTO", description = "Budget response")
+public class BudgetResponseDTO {
+
+    @Schema(description = "Budget id", example = "1")
+    private Long id;
+    @Schema(description = "Budget name", example = "Groceries")
+    private String name;
+    @Schema(description = "Start date of budget", example = "2023-08-01")
+    private LocalDate startDate;
+    @Schema(description = "End date of budget", example = "2023-08-31")
+    private LocalDate endDate;
+    @Schema(description = "Total budget amount", example = "1000.00")
+    private BigDecimal totalBudget;
+    @Schema(description = "Budget status", example = "ACTIVE")
+    private String status;
+    @Schema(description = "createdAt of budget", example = "2023-08-01")
+    private LocalDate createdAt;
+    @Schema(description = "updatedAt of budget", example = "2023-08-01")
+    private LocalDate updatedAt;
+
+}
---
+++ src/main/java/com/derrick/finlypal/service/BudgetService.java
@@ -0,0 +1,4 @@
+package com.derrick.finlypal.service;
+
+public interface BudgetService {
+}
