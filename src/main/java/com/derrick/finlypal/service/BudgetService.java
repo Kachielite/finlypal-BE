94322b9c44d4873cc7ae19f21d5b3b1dc1c2f198
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-12 05:43:50 +0300

    feat(budget): add budget service

--- src/main/java/com/derrick/finlypal/service/BudgetService.java
+++ src/main/java/com/derrick/finlypal/service/BudgetService.java
@@ -1,3 +1,17 @@
 package com.derrick.finlypal.service;
 
-public interface BudgetService {}
+import com.derrick.finlypal.dto.BudgetRequestDTO;
+import com.derrick.finlypal.dto.BudgetResponseDTO;
+import org.springframework.data.domain.Page;
+
+public interface BudgetService {
+    BudgetResponseDTO createBudget(BudgetRequestDTO budgetRequestDTO);
+
+    BudgetResponseDTO updateBudget(Long budgetId, BudgetRequestDTO budgetRequestDTO);
+
+    BudgetResponseDTO getBudgetById(Long budgetId);
+
+    Page<BudgetResponseDTO> getAllBudgets(int page, int pageSize);
+
+    void deleteBudget(Long budgetId);
+}
