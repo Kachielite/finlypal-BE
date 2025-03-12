fe5eb74c24b341dcbdb807beee9845ba73f117bd
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-12 05:57:24 +0300

    feat(budget): add savings service

---
+++ src/main/java/com/derrick/finlypal/service/SavingsService.java
@@ -0,0 +1,18 @@
+package com.derrick.finlypal.service;
+
+import com.derrick.finlypal.dto.SavingsRequestDTO;
+import com.derrick.finlypal.dto.SavingsResponseDTO;
+import org.springframework.data.domain.Page;
+
+public interface SavingsService {
+
+    SavingsResponseDTO createSavings(SavingsRequestDTO savingsRequestDTO);
+
+    SavingsResponseDTO updateSavings(Long savingsId, SavingsRequestDTO savingsRequestDTO);
+
+    SavingsResponseDTO getSavingsById(Long savingsId);
+
+    Page<SavingsResponseDTO> getAllSavings(int page, int pageSize);
+
+    void deleteSavings(Long savingsId);
+}
